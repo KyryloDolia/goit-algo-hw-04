f1e5a80d7a1dcaa61b1504b940f35d632778c5a7
KyryloDolia <fr1ghtn1ght2101@gmail.com>
2024-07-23 02:04:33 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -38,14 +38,19 @@ def get_cats_info(path: str) -> list:
 
     cat_dicts = []
     with open(path, 'r', encoding='utf-8') as fh:
-
         lines = [el.strip().split(',') for el in fh.readlines()]
         for line in lines:
             try:
-                cat_dicts.append({'id': line[0], 'name': line[1], 'age': int(line[2])})
+                cat_id = line[0]
+                cat_name = line[1]
+                cat_age = int(line[2])
+                if cat_age < 0:
+                    print(f"Некоректний вік у рядку: {lines.index(line) + 1}")
+                    continue
+                cat_dicts.append({'id': cat_id, 'name': cat_name, 'age': cat_age})
             except (IndexError, ValueError):
-                print(f"Некоректні дані в рядку: {lines.index(line)+1}")  
-                
+                print(f"Некоректні дані в рядку: {lines.index(line) + 1}")
+
     return cat_dicts
 
 
