9d852a466efb7138b4bb4d13a303d73812590e45
KyryloDolia <fr1ghtn1ght2101@gmail.com>
2024-07-22 20:18:07 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,28 +1,7 @@
 from pathlib import Path
 
-def get_cats_info(path: str) -> list:
-    
-    path = Path(path)
-    if not path.exists():
-        print("Файл за вказаним шляхом не знайдений")
-        return None
-
-    cat_dicts = []
-    with open(path, 'r', encoding='utf-8') as fh:
-
-        lines = [el.strip().split(',') for el in fh.readlines()]
-        for line in lines:
-            try:
-                cat_dicts.append({'id': line[0], 'name': line[1], 'age': int(line[2])})
-            except (IndexError, ValueError):
-                print(f"Невірні дані в рядку: {lines.index(line)+1}")  
-                
-    return cat_dicts
-
-
 
 def total_salary(path: str) -> tuple:
-    
     path = Path(path)
     if not path.exists():
         print("Файл за вказаним шляхом не знайдений")
@@ -48,3 +27,27 @@ def total_salary(path: str) -> tuple:
         summ = sum(salaries)
         avg = summ // len(salaries)
         return summ, avg
+
+
+
+def get_cats_info(path: str) -> list:
+    path = Path(path)
+    if not path.exists():
+        print("Файл за вказаним шляхом не знайдений")
+        return None
+
+    cat_dicts = []
+    with open(path, 'r', encoding='utf-8') as fh:
+
+        lines = [el.strip().split(',') for el in fh.readlines()]
+        for line in lines:
+            try:
+                cat_dicts.append({'id': line[0], 'name': line[1], 'age': int(line[2])})
+            except (IndexError, ValueError):
+                print(f"Невірні дані в рядку: {lines.index(line)+1}")  
+                
+    return cat_dicts
+
+
+
+
